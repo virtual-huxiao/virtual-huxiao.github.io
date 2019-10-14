0c5320f8746727ffa31d4f162bf4d7189a9ac2f2
virtual-huxiao <46273518+virtual-huxiao@users.noreply.github.com>
2019-10-14 23:58:32 +0800

    Add files via upload

---
+++ js/moon_cool.js
@@ -0,0 +1,47 @@
+
+// 254,194,81  -> 0.1089, 0.6811, 0.9961
+// 234,142,57  -> 0.08, 0.7564, 0.9176
+
+// 225,224,229  ->  0.5778,0.0588,1.0000
+
+let ctx;
+
+function setup(){
+  ctx = createCanvas(600, 600).canvas.getContext('2d');
+  //应该使用的是HSB/HSV模式的颜色，只是调节亮度达到月亮的效果
+}
+
+function draw(){
+  let b = 1;
+  let xMult = 0.01;
+  let yMult = 0.01;
+  background(0);
+  //绘制背景之后立刻进行绘制光晕
+  push();
+  colorMode(RGB);
+  ctx.shadowColor = 'rgba(225,224,229,0.8)';
+  ctx.shadowBlur = 35;
+  fill(225,224,229);//没有填充色的话是不会存在阴影的
+  noStroke();
+  ellipse(width/2, height/2, height-50);
+  pop();
+
+  //进行月亮的计算
+  push();
+  colorMode(HSB, 1, 1, 1, 1);  
+  noFill();
+  noStroke();
+  ctx.arc(width/2, height/2, height/2-50,0,PI*2,false);
+  ctx.clip();
+  for(let i = 0; i<width; ++i){
+    for(let j = 0; j<height; ++j ){
+      stroke(0.5778,0.0588,constrain(map(noise(i*xMult,j*yMult),0,1,0.55,1.2), 0, 1));
+      point(i, j);
+    }
+  }
+  pop();
+
+
+  noLoop();
+}
+
